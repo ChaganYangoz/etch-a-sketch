39bb8e96f8740fe2af03cbeaa365f83c7490fd38
ChaganYangoz <caganyangoz@gmail.com>
2023-02-28 17:54:58 +0300

    added size protection

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 let count=0;
+let currentMode="colorMode";
 const container = document.getElementById("container");
 const btn = document.querySelector('#reset');
 const apply=document.querySelector('#apply');
+const colorChoice=document.querySelector('#colorpicker');
+const eraser=document.querySelector('#eraser');
+const rainbow=document.querySelector('#rainbow');
 
 let mouseDown = false;
 container.onmousedown = () => mouseDown = true;
 container.onmouseup = () => mouseDown = false;
 
+
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    let size = 250 / pixel;
     count++;
-    cell.style.padding = size + 'px';
+    cell.style.maxWidth = container.clientWidth/rows+"px";
+    cell.style.maxHeight=container.clientWidth/rows+"px";
     cell.addEventListener("mousedown", changeColor);
     cell.addEventListener("mouseover", changeColor);
-   /* cell.addEventListener('click', function (e) {
-      e.target.style.background = 'black';
-    });*/
     container.appendChild(cell).className = "grid-item";
   };
+  container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
 };
 
 btn.onclick = () => document.location.reload();
@@ -47,10 +49,23 @@ function deleteRows(count){
   } 
 }
 
+eraser.onclick=()=> currentMode="eraser";
+rainbow.onclick=()=>currentMode="rainbow";
+colorChoice.onclick=()=>currentMode="colorMode";
+
 function changeColor(e){
   if (e.type == "mouseover" && !mouseDown) return;
 
-  e.target.style.backgroundColor = "black";
+  if(currentMode=="colorMode"){
+    e.target.style.backgroundColor = colorChoice.value;
+  }else if(currentMode=="rainbow"){
+    let r=Math.floor(Math.random() * 256);
+    let g=Math.floor(Math.random() * 256);
+    let b=Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor=`rgb(${r}, ${g}, ${b})`;
+  }else if(currentMode=="eraser"){
+    e.target.style.backgroundColor="white";
+  }
 }
 
 makeRows(pixel, pixel);
